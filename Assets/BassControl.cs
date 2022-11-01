9f368a61b80ecfdf4195c81f1f8a5cd36b20fa29
Arthur <arthur.pointeau@gmail.com>
2022-11-01 13:48:21 +0100

    more content

---
+++ Assets/BassControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BassControl : MonoBehaviour
+{
+    private MeshRenderer[] _planes;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _planes = transform.GetComponentsInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    public void Bass(float val)
+    {
+        foreach (var plane in _planes)
+        {
+            plane.material.SetFloat("Vector1_B71FBE90", val);
+        }
+    }
+}
